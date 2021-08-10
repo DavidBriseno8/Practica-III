a3a25525d04798e0e203d5e9d7fd40b083516f67
DavidBriseno8 <85203216+DavidBriseno8@users.noreply.github.com>
2021-08-09 23:38:41 -0500

    source Practica III

---
+++ TEMPERATURE_DRIVER.h
@@ -0,0 +1,13 @@
+/*
+ * TEMPERATURE_DRIVER.h
+ *
+ *  Created on: 22 jun. 2021
+ *      Author: DavidE
+ */
+
+#ifndef TEMPERATURE_DRIVER_H_
+#define TEMPERATURE_DRIVER_H_
+
+extern void taketemperature(void);
+
+#endif /* TEMPERATURE_DRIVER_H_ */
